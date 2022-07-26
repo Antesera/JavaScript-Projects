d51fc6039afeb120e2e0b4e4fb4b212b005e35f1
Antesera <107664612+Antesera@users.noreply.github.com>
2022-07-26 05:34:55 -0400

    Add files via upload

---
+++ Basic -Javascript-project/project- expression-alert/operators.js
@@ -0,0 +1,12 @@
+
+var topic = "javascript variables!";
+console.log(topic);
+topic = "update value!";
+console.log(topic);
+
+var likes = 0;
+likes += 1000;
+likes += 100
+var comment = 200
+var totalReaction = likes + comments;
+console.log(likes)
