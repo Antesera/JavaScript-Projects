3577671393221735717b9cefc89271b75966c4b5
Antesera <107664612+Antesera@users.noreply.github.com>
2022-07-27 22:14:59 -0400

    Add files via upload

---
+++ Function.js
@@ -0,0 +1,25 @@
+/*
+concat--> merge
+slice --> slice piece of array to a new array  */
+var user1 = ["Ant", "sera"];
+var user2 = ["Hela", "sera"];
+var user3 = ["Sera", "Hela"];
+
+console.log(user1);
+console.log(user2);
+console.log(user3);
+var users = user1.concat(user2);
+console.log(users);
+users.splice(3, 2, "Get", "Tekma");
+console.log(users);
+console.log(x);
+var T = users.slice(-1, 3);
+var p = 30;
+var pv = "Ant";
+console.log(users);
+console.log(T)
+console.log(array.isArray(T));
+
+         
+
+
